619167d144592445003a98bd2517db2f60181b2a
Lays Rodrigues <laysrodriguessilva@gmail.com>
2016-11-03 22:35:47 -0200

    Initialize DB
    Add persistency of the subscription data on the DataBase.
    
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -4,8 +4,8 @@ from django.core import mail
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.template.loader import render_to_string
-
 from eventex.subscriptions.forms import SubscriptionForm
+from eventex.subscriptions.models import Subscription
 
 
 def subscribe(request):
@@ -26,7 +26,7 @@ def create(request):
                'subscriptions/subscription_email.txt',
                form.cleaned_data
                )
-
+    Subscription.objects.create(**form.cleaned_data)
     messages.success(request, 'Inscrição realizada com sucesso!')
     return HttpResponseRedirect('/inscricao/')
 
